0bb452b78ef3fc796d923ddb011c4e91d154fe5e
unknown <a1149775690@gmail.com>
2023-03-12 21:34:59 +0800

    窗口创建

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
+int clearColorBuffer();
 
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
@@ -16,17 +17,17 @@ int main()
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // opengl 模型 核心模式
 
-  GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+  GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL); // 窗口创建
   if (window == NULL)
   {
     std::cout << "Failed to create GLFW window" << std::endl;
-    glfwTerminate();
+    glfwTerminate(); // 结束线程
     return -1;
   }
   glfwMakeContextCurrent(window);
-  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); // 注册窗口监听 第一次显示同时调用
 
-  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) // 判断 GLAD 是否初始化成功
   {
     std::cout << "Failed to initialize GLAD" << std::endl;
     return -1;
@@ -35,6 +36,7 @@ int main()
   while (!glfwWindowShouldClose(window))
   {
     processInput(window);
+    clearColorBuffer();
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
@@ -45,11 +47,19 @@ int main()
 
 void processInput(GLFWwindow *window)
 {
-  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+  // std::cout << "" << std::endl;
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) // 如果有按键输入 esc 关闭窗口
     glfwSetWindowShouldClose(window, true);
 }
 
-void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) // 窗口改变回调
 {
-  glViewport(0, 0, width, height);
+  glViewport(0, 0, width, height); // 设置大小显示数据和坐标 gl 前两个参数控制窗口左下角的位置   范围映射（0，1）
+}
+
+int clearColorBuffer()
+{
+  glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+  glClear(GL_COLOR_BUFFER_BIT);
+  return 0;
 }
