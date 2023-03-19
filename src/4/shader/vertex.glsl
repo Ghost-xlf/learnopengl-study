2d8d3031a2a40336abe7e94b8ce42c440070f1ba
unknown <a1149775690@gmail.com>
2023-03-19 19:21:20 +0800

    纹理读取

---
+++ src/4/shader/vertex.glsl
@@ -0,0 +1,12 @@
+#version 330 core
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aColor; // 颜色变量的属性位置值为 1
+layout (location = 2) in vec2 aTexCoord; // 颜色变量的属性位置值为 1
+out vec3 outColor;
+out vec2 texCoord;
+uniform float xOffSize;
+void main() {
+  gl_Position = vec4(aPos.x + xOffSize, aPos.y, aPos.z, 1.0f);
+  outColor = aColor;
+  texCoord = aTexCoord;
+};
